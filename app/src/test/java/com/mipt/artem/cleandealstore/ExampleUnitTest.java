9400ca6ce9a6cd1cf207a450c970e74e0bff6780
artem <artem.rudoj@frtk.ru>
2016-12-20 23:35:31 +0300

    add example tests

--- app/src/test/java/com/mipt/artem/cleandealstore/ExampleUnitTest.java
+++ app/src/test/java/com/mipt/artem/cleandealstore/ExampleUnitTest.java
@@ -2,6 +2,10 @@ package com.mipt.artem.cleandealstore;
 
 import org.junit.Test;
 
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+
 import static org.junit.Assert.*;
 
 /**
@@ -11,5 +15,14 @@ public class ExampleUnitTest {
     @Test
     public void addition_isCorrect() throws Exception {
         assertEquals(4, 2 + 2);
+    }
+
+    @Test
+    public void OutputStreamWriter_Closes_OutputStream_on_Close()
+            throws IOException {
+        OutputStream mock = mock(OutputStream.class);
+        OutputStreamWriter osw = new OutputStreamWriter(mock);
+        osw.close();
+        verify(mock).close();
     }
 }
